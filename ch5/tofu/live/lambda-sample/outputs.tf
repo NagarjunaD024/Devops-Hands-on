9e4f9ee6ec1706658245d288d37fdd8e7067d93b
Nagarjuna Gottipati <64250120+NagarjunaD024@users.noreply.github.com>
2025-09-30 11:47:13 +0000

    changed outputs.tf

--- ch5/tofu/live/lambda-sample/outputs.tf
+++ ch5/tofu/live/lambda-sample/outputs.tf
@@ -2,3 +2,13 @@ output "function_url" {
   description = "The URL of the Lambda function"
   value       = module.function.function_url
 }
+
+output "lambda_deploy_plan_role_arn" {
+  description = "The ARN of the IAM role for plan"
+  value       = module.iam_roles.lambda_deploy_plan_role_arn
+}
+
+output "lambda_deploy_apply_role_arn" {
+  description = "The ARN of the IAM role for apply"
+  value       = module.iam_roles.lambda_deploy_apply_role_arn
+}
