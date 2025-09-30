d3e14abc5a45681542bebd199ed5b7c4e8dbe235
Nagarjuna Gottipati <64250120+NagarjunaD024@users.noreply.github.com>
2025-09-30 11:56:42 +0000

    Add plan and apply workflows

--- ch5/tofu/live/ci-cd-permissions/outputs.tf
+++ ch5/tofu/live/ci-cd-permissions/outputs.tf
@@ -1,14 +1,14 @@
 output "lambda_test_role_arn" {
   description = "The ARN of the IAM role for testing"
-  value       = module.iam_roles.lambda_test_role_arn
+  value       = arn:aws:iam::198321609771:role/lambda-sample-tests
 }
 
 output "lambda_deploy_plan_role_arn" {
   description = "The ARN of the IAM role for plan"
-  value       = module.iam_roles.lambda_deploy_plan_role_arn
+  value       = arn:aws:iam::198321609771:role/lambda-sample-plan
 }
 
 output "lambda_deploy_apply_role_arn" {
   description = "The ARN of the IAM role for apply"
-  value       = module.iam_roles.lambda_deploy_apply_role_arn
+  value       = arn:aws:iam::198321609771:role/lambda-sample-apply
 }
